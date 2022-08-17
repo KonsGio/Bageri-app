4311f7e4ae017491c2c2513cc58bfea1ce87debd
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-08-17 18:54:08 +0300

    Cart popup position

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -53,7 +53,7 @@ const Header = () => {
           </ul>
           <div className='relative flex items-center justify-center'>
             <MdShoppingBasket className='text-gray-600 text-2xl ml-8 cursor-pointer'/>
-            <div className='absolute -right-2 -top-2 w-5 h-5 rounded-full bg-red-600 flex items-center justify-center'>
+            <div className='absolute -right-3 -top-3 w-5 h-5 rounded-full bg-red-600 flex items-center justify-center'>
               <p className='text-xs text-white font-semibold'>2</p>
             </div>
           </div>
