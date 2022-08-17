6fec32d7beb912b5a2c490bfc1c51487f877105d
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-08-17 14:38:18 +0300

    Basic header component complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
-import Logo from '../img/logo.png'
 import {MdShoppingBasket} from 'react-icons/md'
+
+import Logo from '../img/logo.png'
+import Avatar from '../img/avatar.png'
 
 const Header = () => {
   return (
     <header className='fixed z-50 w-screen p-6 px-16'>
         {/* Desktop,tablet */}
-        <div className='hidden md:flex w-full h-full '>
+        <div className='hidden md:flex w-full h-full items-center justify-between'>
           <div className='flex items-center gap-2'>
-            <img src={Logo} className='w-28 object-cover' alt ='logo'/>
-            <p className='text-slate-500 text-xl font-bold'></p>
+            <img src={Logo} className='w-24 object-cover' alt ='logo'/>
+            <p className='text-headingColor text-xl font-bold'></p>
           </div>
-          <ul className='flex items-center gap-8 ml-auto'>
-            <li className='text-base text-gray-800 hover:text-slate-500 duration-100 transition-all ease-in-out cursor-pointer'>Home</li>
-            <li className='text-base text-gray-800 hover:text-slate-500 duration-100 transition-all ease-in-out cursor-pointer'>Menu</li>
-            <li className='text-base text-gray-800 hover:text-slate-500 duration-100 transition-all ease-in-out cursor-pointer'>About Us</li>
-            <li className='text-base text-gray-800 hover:text-slate-500 duration-100 transition-all ease-in-out cursor-pointer'>Services</li>
+          <div className='flex items-center'>
+          <ul className='flex items-center gap-8'>
+            <li className='text-base text-textColor hover:text- transition-all ease-in-out cursor-pointer'>Home</li>
+            <li className='text-base text-textColor hover:text-headingColor transition-all ease-in-out cursor-pointer'>Menu</li>
+            <li className='text-base text-textColor hover:text-headingColor transition-all ease-in-out cursor-pointer'>About Us</li>
+            <li className='text-base text-textColor hover:text-headingColor transition-all ease-in-out cursor-pointer'>Services</li>
           </ul>
           <div className='relative flex items-center justify-center'>
-            <MdShoppingBasket className='text-gray-600 text-2xl ml-8'/>
-            <div className='w-10 h-10 rounded-full bg-cartBg'>
-
+            <MdShoppingBasket className='text-gray-600 text-2xl ml-8 cursor-pointer'/>
+            <div className='absolute -right-2 -top-4 w-5 h-5 rounded-full bg-cartBg flex items-center justify-center'>
+              <p className='text-xs text-white font-semibold'>2</p>
             </div>
+          </div>
+            <img src={Avatar} className='w-10 min-2-[40px] h-10 min-h-[40px] drop-shadow-2xl cursor-pointer' alt='userprofile'/>
           </div>
         </div>
         {/* mobile */}
