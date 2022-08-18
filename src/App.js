49f1591f54adedbc33e743ad5c681f5671e83c74
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-08-18 15:59:54 +0300

    Main container grid

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     <div className='w-screen h-auto flex flex-col bg-primary'>
       <Header/>
 
-      <main className='mt-24 p-8 w-full'>
+      <main className='mt-24 p-20 md:px-16 py-8 w-full'>
       
         <Routes>
           <Route path='/*' element={<MainContainer/>}/>
