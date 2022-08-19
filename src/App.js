52e8f19b2580af6847fe8da7c4eab1e74ffed797
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-08-19 13:08:01 +0300

    App px and md:mt changes

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import { AnimatePresence } from 'framer-motion';
 const App = () => {
   return (
     <AnimatePresence exitBeforeEnter>
-    <div className='w-screen h-auto flex flex-col bg-primary'>
+    <div className='w-screen h-auto flex flex-col'>
       <Header/>
 
-      <main className='mt-24 p-20 md:px-16 py-8 w-full'>
+      <main className='mt-24 md:mt-20 px-4 md:px-16 py-4 w-full'>
       
         <Routes>
           <Route path='/*' element={<MainContainer/>}/>
