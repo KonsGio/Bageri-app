1baef69fed50b7de2ec9acecefa88da68a07f519
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-08-20 17:56:08 +0300

    Fetch data from database

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Header,MainContainer,CreateContainer } from './components';
 import {Routes, Route} from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import { useStateValue } from './context/StateProvider';
+import { getAllFoodItems } from './utils/firebaseFunctions';
+import { actionType } from './context/reducer';
 
 
 const App = () => {
+  const [{fooItems}, dispatch] = useStateValue();
+
+  const fetchData = async () => {
+    await getAllFoodItems() .then(data => {
+      dispatch({
+        type: actionType.SET_FOOD_ITEMS,
+        fooItems: data
+      });
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <AnimatePresence exitBeforeEnter>
     <div className='w-screen h-auto flex flex-col'>
