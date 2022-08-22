0627ec0b53f15169be947b2ebbc57a93ff5cec6b
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-08-22 17:22:35 +0300

    Empty category message added

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { MdShoppingBasket } from "react-icons/md";
 import { motion } from "framer-motion";
-
+import NotFound from '../img/NotFound.svg'
 const RowContainer = ({flag, data, scrollValue}) => {
 
   const rowContainer = useRef();
@@ -20,7 +20,8 @@ const RowContainer = ({flag, data, scrollValue}) => {
     }`}
   >
         
-        {data && data.map((item) => (
+        {data && data.length > 0 ? 
+         (data.map((item) => (
           <div key={item?.id} className='w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px]  bg-cardOverlay rounded-lg py-2 px-4  my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative'>
             <div className='w-full flex items-center justify-between'>
               <motion.div
@@ -53,8 +54,14 @@ const RowContainer = ({flag, data, scrollValue}) => {
             </div>
           </div>
        </div>
-        ))}
-
+        ))) : (
+          <div className="w-full flex flex-col items-center justify-center">
+            <img src={NotFound} className="h-340" />
+            <p className="text-xl text-headingColor font-semibold my-2">
+             Δεν υπάρχουν διαθέσιμα προιόντα
+           </p>
+        </div>
+        )}
     </div>
   )
 };
