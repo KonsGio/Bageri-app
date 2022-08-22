807701db686fb4d512be1ce5534272b08525d276
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-08-22 17:14:35 +0300

    Row container fixed margins

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -23,21 +23,27 @@ const RowContainer = ({flag, data, scrollValue}) => {
         {data && data.map((item) => (
           <div key={item?.id} className='w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px]  bg-cardOverlay rounded-lg py-2 px-4  my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative'>
             <div className='w-full flex items-center justify-between'>
-              <motion.img
-                whileHover={{ scale: 1.1 }}
-                src={item?.imageURL}
-                className='w-40 -mt-8 drop-shadow-2xl' alt=''/>
+              <motion.div
+                className="w-40 h-40 drop-shadow-2xl"
+                whileHover={{ scale: 1.2 }}
+              >
+                <img
+                  src={item?.imageURL}
+                  alt=""
+                  className="w-full h-full object-contain"
+                />
+              </motion.div>
               <motion.div 
                 whileTap={{ scale: 0.75 }}
-                className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md">
+                className="w-8 h-8 rounded-full bg-red-600 flex items-center justify-center cursor-pointer hover:shadow-md -mt-8">
                   <MdShoppingBasket className="text-white" />
               </motion.div>
             </div>
             <div className='w-full flex flex-col items-end justify-end'>
-              <p className="text-textColor font-semibold text-base md:text-lg">
+              <p className="text-textColor font-semibold text-base md:text-lg ">
                 {item?.title}
               </p>
-              <p className="mt-1 text-sm text-gray-500">
+              <p className="mt-1 text-sm text-gray-500 ">
                 {item?.calories} Θερμίδες
               </p>
             <div className="flex items-center gap-8">
