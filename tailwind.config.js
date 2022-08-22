5eee1d0495df369972faa66b81ebcf94a01b7569
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-08-22 16:24:13 +0300

    Added tailwind scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,8 +55,9 @@ module.exports = {
         cartBg: "#282a2c",
         cartItem: "#2e3033",
         cartTotal: "#343739",
+        rowBg:'rgba(255,131,0,0.2)',
       },
     },
   },
-  // plugins: [require("tailwind-scrollbar")],
+  plugins: [require("tailwind-scrollbar")],
 };
