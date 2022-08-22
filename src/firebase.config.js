5a0d97ca2d8a2549a2789b22192d35b6216f9eba
KonsGio <88385243+KonsGio@users.noreply.github.com>
2022-08-23 02:23:43 +0300

    ENV variables

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,13 @@ import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCDADphHbWB6KNPzdFsk5pY5V5oDIrGRb4",
-    authDomain: "bageri-b9cb3.firebaseapp.com",
-    databaseURL: "https://bageri-b9cb3-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "bageri-b9cb3",
-    storageBucket: "bageri-b9cb3.appspot.com",
-    messagingSenderId: "1075574334758",
-    appId: "1:1075574334758:web:bc4de202cf7de1cfe24de7",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   };
 
   const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
